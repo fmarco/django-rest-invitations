709de8ea10200c7b47037424a2419168e738a883
Artem Dumanov <artem.dymanov@gmail.com>
2020-05-16 18:54:37 +0300

    Fixed urls to consume views from settings

--- rest_invitations/urls.py
+++ rest_invitations/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from rest_framework import routers
 
 from .app_settings import ACCEPT_INVITE_URL, API_BASE_URL
-from .views import InvitationViewSet, accept_invitation
+from .app_settings import (InvitationViewSet, accept_invitation)
 
 router = routers.SimpleRouter()
 router.register(r'{0}'.format(API_BASE_URL), InvitationViewSet)
